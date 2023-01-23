1f0df42d49ddf1f7a3fad2d9177dd45cacebad48
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2023-01-23 08:44:08 -0600

    Added comments to Javascript file.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Testing to see if the counter is working and initialising count 
 let count = 0
 console.log(count)
 
+// Welcoming user to the site
 let welcomeEl = document.getElementById("welcome-el")
 let name = prompt("What is your name? ") + " "
 let greeting = "Welcome back, "
 let welcome = welcomeEl.textContent = greeting + name + "\u{0263A}"
 
+// Making functions to increase and decrease the count
 function increment() {
     count = count + 1
     document.getElementById("count").textContent = count
@@ -16,6 +19,7 @@ function decrement() {
     document.getElementById("count").textContent = count
 }
 
+// Saving the count and printing a log with a date.
 function save() {
     console.log(count)
     let time = new Date()
